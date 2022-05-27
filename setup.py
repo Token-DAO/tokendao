14dd593b08862b93101499a67f8de198425405a5
TokenDAO <tokendao.quant@gmail.com>
2022-05-26 19:58:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -147,15 +147,15 @@ setup(
     },
     # If there are data files included in your packages that need to be
     # installed, specify them here.
-    package_data={  # Optional
-        "sample": ["package_data.dat"],
-    },
+    # package_data={  # Optional
+    #     "sample": ["package_data.dat"],
+    # },
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/distutils/setupscript.html#installing-additional-files
     #
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[("my_data", ["data/data_file"])],  # Optional
+    # data_files=[("my_data", ["data/data_file"])],  # Optional
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # `pip` to create the appropriate form of executable for the target
@@ -163,11 +163,11 @@ setup(
     #
     # For example, the following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
-    entry_points={  # Optional
-        "console_scripts": [
-            "sample=sample:main",
-        ],
-    },
+    # entry_points={  # Optional
+    #     "console_scripts": [
+    #         "sample=sample:main",
+    #     ],
+    # },
     # List additional URLs that are relevant to your project as a dict.
     #
     # This field corresponds to the "Project-URL" metadata fields:
